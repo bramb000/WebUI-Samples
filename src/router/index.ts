0f90edaf8d11c3aeb3a1b6f18c3f5107d25f9873
bramb000 <55013431+bramb000@users.noreply.github.com>
2026-02-22 14:08:39 +1100

    feat: Configure deployment pipeline, adjust Vite base path, and add application routing.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
@@ -62,7 +62,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory('/WebUI-Samples/'),
+    history: createWebHistory(),
     routes
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  base: '/bramha-personal-portfolio/',
+  base: '/',
   build: {
     outDir: 'build',
   },
